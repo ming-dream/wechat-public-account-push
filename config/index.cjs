cab80642bd4219a1b63dac9e92d25dc644d41fd9
zhao <920433768@qq.com>
2022-09-25 21:38:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,13 +67,13 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oT5x75i7djx0yB-iFfKZRmiJEjFY',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '1yOagfHD530bnrrWS29xzaPQN6ByBBNFvAuqsnPzrzE',
+      useTemplateId: 'uLarfD7ddfnLUkL8skFjJGEepIlhpQOoxcko7dWd-1g',
       // 所在省份
       province: '上海',
       // 所在城市
       city: '上海',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '12-01',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,7 +82,7 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '*生日', name: '老婆', year: '1999', date: '12-01',
         },
         {
           type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
@@ -98,7 +98,7 @@ const USER_CONFIG = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2021-04-03' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
         // 退伍日
